544fe855da01718e8589d7c13ff6489625e2f2e3
fanlu91 <fanlu91@gmail.com>
2019-06-04 15:20:58 +0800

    best solution for sort array by parity I & II

--- src/array/SortArrayByParity.java
+++ src/array/SortArrayByParity.java
@@ -4,44 +4,34 @@ package array;
 // Author : Fanlu Hai
 // Date   : 2018-04-15
 // Topic  : Array
-// Result : 51.52% 38.56%
+// Result : 100.00% 96.91%
 
-import java.util.Arrays;
 
 class SortArrayByParity {
+
     public int[] sortArrayByParity(int[] A) {
         int i = 0;
         int j = A.length - 1;
+        int tmp;
+
         while (j > i) {
-//            System.out.println(Arrays.toString(A));
+            // do swap
             if (A[i] % 2 == 1 && A[j] % 2 == 0) {
-                swapNumInArray(i, j, A);
+                tmp = A[i];
+                A[i] = A[j];
+                A[j] = tmp;
             }
 
             if (A[i] % 2 == 0) {
                 i++;
                 //in order to make sure a check is performed
                 continue;
-            }
-
-            if (A[j] % 2 == 1) {
+            } else if (A[j] % 2 == 1) {
                 j--;
                 //in order to make sure a check is performed
                 continue;
             }
         }
         return A;
-    }
-
-    public void swapNumInArray(int firstIndex, int secondIndex, int[] array) {
-        int tmp = array[firstIndex];
-        array[firstIndex] = array[secondIndex];
-        array[secondIndex] = tmp;
-    }
-
-    public static void main(String[] args) {
-        SortArrayByParity sortArrayByParity = new SortArrayByParity();
-        int[] a = {1, 1, 1, 1, 2, 2, 2, 3, 3, 33, 4, 4, 5, 5, 5, 5, 6, 6, 7, 7, 7, 8};
-        System.out.println(Arrays.toString(sortArrayByParity.sortArrayByParity(a)));
     }
 }
--- src/array/SortArrayByParityII.java
+++ src/array/SortArrayByParityII.java
@@ -4,39 +4,36 @@ package array;
 // Author : Fanlu Hai
 // Date   : 2018-04-15
 // Topic  : Array
-// Result : 65.92% 95.94%
+// Result : 99.72% 99.43%
 
-import java.util.Arrays;
 
 class SortArrayByParityII {
-    public int[] sortArrayByParityII(int[] A) {
+
+    //60.45% 3 ms 99.32%  40.4 MB
+    public int[] sortArrayByParityIIOrigin(int[] A) {
         int i = 0;
         int j = 1;
+        // below code hasn't take full advantage of half odd half even situation
+        // if do check before swap which can be improved under above condition
+        // see improved code in the next function
         while (true) {
-//            System.out.println(A.length + " " + i + " " + j + " " + Arrays.toString(A));
-
             if (i >= A.length || j >= A.length) {
                 return A;
             }
-
             if (A[i] % 2 == 1 && A[j] % 2 == 0) {
                 swapNumInArray(i, j, A);
             }
-
             if (A[i] % 2 == 0) {
                 i += 2;
                 //in order to make sure a check is performed
                 continue;
             }
-
             if (A[j] % 2 == 1) {
                 j += 2;
                 //in order to make sure a check is performed
                 continue;
             }
-
         }
-
     }
 
     public void swapNumInArray(int firstIndex, int secondIndex, int[] array) {
@@ -45,9 +42,22 @@ class SortArrayByParityII {
         array[secondIndex] = tmp;
     }
 
-    public static void main(String[] args) {
-        SortArrayByParityII sortArrayByParityII = new SortArrayByParityII();
-        int[] a = {1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 2, 2};
-        System.out.println(Arrays.toString(sortArrayByParityII.sortArrayByParityII(a)));
+    // 99.72% 99.43%
+    public int[] sortArrayByParityII(int[] A) {
+        int i = 0;
+        int j = 1;
+        int tmp;
+        while (i < A.length) {
+            if ((A[i] & 0x1) == 1) {
+                while ((A[j] & 0x1) == 1) {
+                    j += 2;
+                }
+                tmp = A[i];
+                A[i] = A[j];
+                A[j] = tmp;
+            }
+            i += 2;
+        }
+        return A;
     }
 }
